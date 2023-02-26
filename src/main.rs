aaaa75289931861f835b1377ad24f4766eaff65b
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2023-02-27 00:38:36 +0500

    [FINISH] success

--- src/main.rs
+++ src/main.rs
@@ -8,71 +8,77 @@ use std::env;
 use std::io::{stdin, stdout, Write};
 
 #[derive(Deserialize, Debug)]
-
-struct RuneoResponse{
+struct RuneoResponse {
     id: Option<String>,
     object: Option<String>,
     created: Option<u64>,
     model: Option<String>,
-    choises: Vec<RuneoChoise>,
+    choices: Vec<RuneoChoice>,
 }
 
-
 #[derive(Deserialize, Debug)]
-
-struct RuneoChoise {
-    tetx: String,
+struct RuneoChoice {
+    text: String,
     index: u8,
     logprobs: Option<u8>,
     finish_reason: String,
 }
 
 #[derive(Serialize, Debug)]
-
 struct RuneoRequest {
-    promt: String,
+    prompt: String,
     max_tokens: u16,
 }
 
 #[tokio::main]
-
-async fn main() -> Result<(), Box<dyn std::error::Error + send + Sync>> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     dotenv().ok();
 
     let https = HttpsConnector::new();
 
     let client = Client::builder().build(https);
 
-    let uri = "https://api.openai.com/v1/engines/davinci-codex/completions";
+    let uri = "https://api.openai.com/v1/engines/text-davinci-001/completions";
 
     let preamble = "Salom! Men sizga qanday yordam bera olaman.";
 
     let token: String = env::var("TOKEN").unwrap();
 
-    let auth_header_val = format!("Token: {}", token);
+    let auth_header_val = format!("Bearer {}", token);
 
-    println!("{esc}c", esc = 27 as char)
+    println!("{esc}c", esc = 27 as char);
 
     loop {
-        print!{">"};
+        print!("> ");
         stdout().flush().unwrap();
         let mut user_text = String::new();
 
         stdin()
             .read_line(&mut user_text)
             .expect("Xatolik");
-            println!("");
+        println!("");
 
-        let sp = Spinner::new(&Spinners::Dots12, "\t\tAqqli terminalchaman".into());
-        let runeo_request = RuneoRequest{
-            promt: format!("{} {}", preamble, user_text),
+        let sp = Spinner::new(&Spinners::Dots12, "\t\tAqlli Terminalchaman :)".into());
+
+        let runeo_request = RuneoRequest {
+            prompt: format!("{} {}", preamble, user_text),
             max_tokens: 1000,
         };
 
-        
+        let body = Body::from(serde_json::to_vec(&runeo_request)?);
+        let req = Request::post(uri)
+            .header(header::CONTENT_TYPE, "application/json")
+            .header("Authorization", &auth_header_val)
+            .body(body)
+            .unwrap();
 
+        let res = client.request(req).await?;
+        let body = hyper::body::aggregate(res).await?;
+        sp.stop();
+        println!("");
+        let response: RuneoResponse = serde_json::from_reader(body.reader())?;
+        println!("{}", response.choices[0].text);
     }
 
     Ok(())
-
 }
